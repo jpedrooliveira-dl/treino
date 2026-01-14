cc12f81a6b0e4cc8f2de6826a80836a0c7edce49
joao <tuquinhatuca15@gmail.com>
2026-01-14 20:59:57 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function openTab(evt, tabName) {
+
+    // Esconde todos os conteúdos das abas
+
+    const tabContent = document.getElementsByClassName("tab-content");
+
+    for (let i = 0; i < tabContent.length; i++) {
+
+        tabContent[i].style.display = "none";
+
+        tabContent[i].classList.remove("active");
+
+    }
+
+    // Remove a classe "active" de todos os botões
+
+    const tabBtns = document.getElementsByClassName("tab-btn");
+
+    for (let i = 0; i < tabBtns.length; i++) {
+
+        tabBtns[i].classList.remove("active");
+
+    }
+
+    // Mostra a aba atual e adiciona a classe active ao botão
+
+    document.getElementById(tabName).style.display = "block";
+
+    document.getElementById(tabName).classList.add("active");
+
+    evt.currentTarget.classList.add("active");
+
+}
+
